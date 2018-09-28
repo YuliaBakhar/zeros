aaf9d05c6a3d5017e865df85a9ea3e3620c73f67
YuliaBakhar <bakhar.yulia@gmail.com>
2018-09-29 00:03:03 +0300

    solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function getZerosCount(number) {
   // your implementation
+  let i = 0;
+  let zerosCount  = 0;
+  let fivesExponent = 0; 
+  
+  while (fivesExponent < number){
+    i++;
+    fivesExponent = Math.pow(5, i);
+    zerosCount += Math.floor(number / fivesExponent);
+  }
+  return zerosCount;  
 }
